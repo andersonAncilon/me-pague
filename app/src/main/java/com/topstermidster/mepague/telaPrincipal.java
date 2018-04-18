571109fdb71ff4de309239a134f3592581418887
Anderson Ancilon <anderson@macsedsa04.tre-to.gov.br>
2018-04-18 17:42:21 -0300

    Esse é um aplicativo para controle de finanças, agora você poderá saber quem te deve, desde quando, e se você deve a alguém.

--- app/src/main/java/com/topstermidster/mepague/telaPrincipal.java
+++ app/src/main/java/com/topstermidster/mepague/telaPrincipal.java
@@ -3,16 +3,56 @@ package com.topstermidster.mepague;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class telaPrincipal extends AppCompatActivity {
+
+    private List<devedor> devedorList = new ArrayList<>();
+
+    private RecyclerView rcView;
+    private RecyclerView.Adapter rcAdapter;
+    private RecyclerView.LayoutManager rcLayoutMan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+
+        rcView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        rcAdapter = new devedorAdapter(devedorList);
+        rcLayoutMan = new LinearLayoutManager(getApplicationContext());
+        rcView.setLayoutManager(rcLayoutMan);
+        rcView.setItemAnimator(new DefaultItemAnimator());
+        rcView.setAdapter(rcAdapter);
+
+        prepareDevedorData();
     }
 
+    public void prepareDevedorData() {
+        devedor dev = new devedor("teste", "teste", "teste");
+        devedorList.add(dev);
+
+        dev = new devedor("teste", "teste", "teste");
+        devedorList.add(dev);
+
+        dev = new devedor("teste1", "teste", "teste");
+        devedorList.add(dev);
+
+        dev = new devedor("teste2", "teste", "teste");
+        devedorList.add(dev);
+
+        dev = new devedor("teste3", "teste", "teste");
+        devedorList.add(dev);
+
+        rcAdapter.notifyDataSetChanged();
+    }
 
     public void logoutUser (View view) {
         Intent intent = new Intent(getApplicationContext(), telaLogin.class);
