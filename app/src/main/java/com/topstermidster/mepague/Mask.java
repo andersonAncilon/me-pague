75550beea585f247df90139eead0aebde192c80f
disgraceTerror <dan100anderson@gmail.com>
2018-06-12 16:09:53 -0300

    Correção no recycleView e continuação da exibição dos dados dos devedores em uma nova tela, falta jogar os dados para a mesma.

---
+++ app/src/main/java/com/topstermidster/mepague/Mask.java
@@ -0,0 +1,41 @@
+package com.topstermidster.mepague;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.lang.ref.WeakReference;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
+public class Mask implements TextWatcher {
+    private final WeakReference<EditText> editTextWeakReference;
+
+    public Mask(EditText editText) {
+        editTextWeakReference = new WeakReference<EditText>(editText);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        EditText editText = editTextWeakReference.get();
+        if (editText == null) return;
+        String s = editable.toString();
+        if (s.isEmpty()) return;
+        editText.removeTextChangedListener(this);
+        String cleanString = s.replaceAll("[^0-9]", "");
+        BigDecimal parsed = new BigDecimal(cleanString).setScale(2, BigDecimal.ROUND_FLOOR).divide(new BigDecimal(100), BigDecimal.ROUND_FLOOR);
+        String formatted = NumberFormat.getCurrencyInstance().format(parsed);
+        editText.setText(formatted);
+        editText.setSelection(formatted.length());
+        editText.addTextChangedListener(this);
+    }
+}
---
+++ app/src/main/java/com/topstermidster/mepague/MyMediatorInterface.java
@@ -0,0 +1,6 @@
+package com.topstermidster.mepague;
+
+public interface MyMediatorInterface {
+    void userItemClick(int pos);
+    void userItemClickData(int posx);
+}
